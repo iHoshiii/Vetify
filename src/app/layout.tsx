aa9b523c7392b4c047ea2f6e893ed37134819780
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-06-20 16:07:52 +0800

    feat: add particles in home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
+import SiteHeader from '@/components/SiteHeader';
 import type { Metadata } from 'next';
 import React from 'react';
-import SiteHeader from '@/components/SiteHeader';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Vetify',
   description: "Your pet's health companion",
+  icons: {
+    icon: '/favicon.svg',
+    shortcut: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
